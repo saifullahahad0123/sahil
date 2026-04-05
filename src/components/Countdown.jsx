8500fcc39a9e84fc0bcb141d56b7e45fb0b8e017
saifullah <saifullahahad0@gmail.com>
2026-04-05 22:24:23 +0530

    first commit

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -20,7 +20,9 @@ function Countdown({ onBirthdayReached, birthdayReached }) {
     // ═══════════════════════════════════════════════════════════════
 
     //const targetDate = new Date("2026-04-06-18T00:00:00");
-    const targetDate = new Date("2026-04-06T18:00:00"); 
+    // const targetDate = new Date("2026-04-06-T18:00:00"); 
+    const targetDate = new Date();
+targetDate.setHours(12, 0, 0, 0);
 //const targetDate = new Date(2026, 4, 6, 18, 0, 0);
     // 📝 HOW TO USE:
     // Replace the date above with your actual birthday
